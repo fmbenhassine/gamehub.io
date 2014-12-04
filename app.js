dbafe1d8e26e39cb6fb838365e4ef77d098853a8
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2014-12-04 23:34:11 +0100

    add code comments

--- app.js
+++ app.js
@@ -18,11 +18,27 @@ var app = express();
 // configure database
 require('./config/database')(app, mongoose);
 
-// Bootstrap models
+// bootstrap data models
 fs.readdirSync(__dirname + '/models').forEach(function (file) {
     if (~file.indexOf('.js')) require(__dirname + '/models/' + file);
 });
 
+// configure express app
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser('S3CRE7'));
+app.use(flash());
+app.use(session({ secret: 'S3CRE7-S3SSI0N', saveUninitialized: true, resave: true } ));
+app.use(express.static(path.join(__dirname, 'public')));
+require('./config/passport')(app, passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// configure routes
 var routes = require('./routes/index');
 var account = require('./routes/account');
 var api = require('./routes/api');
@@ -30,30 +46,6 @@ var play = require('./routes/play');
 var login = require('./routes/login');
 var register = require('./routes/register');
 var search = require('./routes/search');
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser('S3CRE7'));
-app.use(flash());
-
-app.use(session(
-    {
-        secret: 'S3CRE7-S3SSI0N',
-        saveUninitialized: true,
-        resave: true
-    }
-));
-
-app.use(express.static(path.join(__dirname, 'public')));
-require('./config/passport')(app, passport);
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', routes);
 app.use('/login', login);
@@ -63,8 +55,10 @@ app.use('/play', play);
 app.use('/api', api);
 app.use('/search', search);
 
+// configure error handlers
 require('./config/errorHandlers.js')(app);
 
+// launch app server
 var server = require('http').createServer(app).listen(3000);
 
 require('./config/socket.js')(server);
